bfe91db73c34727f2c5d1d7da7ac1af922f418c8
Julián <juliandorao@gmail.com>
2025-07-27 23:29:49 -0300

    Implemento useContext para proteger las rutas

--- src/components/RutaProtegida.jsx
+++ src/components/RutaProtegida.jsx
@@ -1,15 +1,14 @@
 import { Navigate } from 'react-router-dom';
+import { useAuthContext } from '../context/AuthContext';
 
-export default function RutaProtegida({ children })
-{
-    const auth = localStorage.getItem('auth') === 'true';
-    return auth ? children : <Navigate to="/Iniciar-sesión" replace/>
-
+export default function RutaProtegida({ children }){
+    const { token } = useAuthContext();
+    return token ? children : <Navigate to="/Iniciar-sesión" replace/>;
 }
 
-// LÓGICA DEL OPERADOR TERNARIO: "Al renderizar, compruebo si el usuario está autenticado (auth). 
-// Si lo está (?), muestro el contenido protegido (children). De lo contrario (:), redirijo al usuario a la página de login (<Navigate to="/login" />)."
+// LÓGICA DEL OPERADOR TERNARIO: "Al renderizar, compruebo si el token del usuario existe, si este esta autentificado. 
+// Si lo está (?), muestro el contenido protegido del componente hijo. De lo contrario (:), redirijo al usuario a la página de login (<Navigate to="/login" />)."
 // Es una manera elegante y declarativa de manejar la lógica de rutas protegidas directamente en el JSX.
 
-// Uso de replace:Al usar <Navigate to="/Iniciar-sesión" replace/>, la ruta /Perfil (que el usuario nunca debió poder ver) es eliminada del historial. Si presiona "Atrás" en la página de login, 
+// Uso de replace: al usar <Navigate to="/Iniciar-sesión" replace/>, la ruta /Perfil (que el usuario nunca debió poder ver) es eliminada del historial. Si presiona "Atrás" en la página de login, 
 // volverá a la página que visitó antes de intentar entrar al perfil, lo cual es la experiencia de usuario que se espera.
